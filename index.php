3b7cc3db61044c4c5589fed76268913cf288690e
jack <yllmvp@163.com>
2024-10-12 18:13:50 +0800

    init

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 $token = "yzd"; // 填写你设置的Token
+
 $signature = $_GET["signature"];
 $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];
-$echostr = $_GET["echostr"];
 
 $tmpArr = array($token, $timestamp, $nonce);
 sort($tmpArr, SORT_STRING);
-$tmpStr = implode($tmpArr);
-$tmpStr = sha1($tmpStr);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
 
-if ($tmpStr == $signature) {
-    echo "success";
-    exit;
-} else {
-    echo "验证失败";
+if( $tmpStr == $signature ){
+    return true;
+}else{
+    return false;
 }
+
 
